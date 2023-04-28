b59532be210667943a8b79145541e17df7d2f98d
cedric-cochard <c.jc@live.fr>
2023-04-28 14:36:38 +0200

    initialization container service

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
+
+private $logger;
+private $calculator;
+
+public function __construct(LoggerInterface $logger, Calculator $calculator)
+{
+  $this->logger = $logger;
+  $this->calculator = $calculator;
+}
   /**
    * Undocumented function
    *@Route("/hello/{firstname}", name="hello")
    */
   public function hello($firstname = "World") {
+
+    $this->logger->error("Mon message de log !");
+    $tva = $this->calculator->calcul(1800);
+    dd($tva);
+
+    
     return new Response("Hello $firstname");
   }
 }
