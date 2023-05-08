4eef8ac7a8081fbf2ddce3fa8e1b47fc02b9df2a
cedric-cochard <c.jc@live.fr>
2023-05-08 20:11:09 +0200

    submission form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,19 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Category;
-use App\Repository\CategoryRepository;
+use App\Entity\Product;
 use App\Repository\ProductRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormFactoryInterface;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
@@ -59,9 +65,12 @@ class ProductController extends AbstractController
      * @Route("/admin/product/create", name="product_create")
      *
      */
-    public function create(FormFactoryInterface $factory) {
+    public function create(FormFactoryInterface $factory, Request $request, SluggerInterface $slugger, EntityManagerInterface $em) {
 
-        $builder = $factory->createBuilder();
+
+        $builder = $factory->createBuilder(FormType::class, null, [
+           "data_class" => Product::class
+        ]);
 
         $builder->add("name", TextType::class, [
             "label" => "Nom du produit",
@@ -75,6 +84,12 @@ class ProductController extends AbstractController
                 "label" => "prix du produit",
                 "attr" => ["placeholder" => "Tapez le prix du produit en €"]
             ])
+            ->add("picture", UrlType::class, [
+                "label" => "Image du produit",
+                "attr" => [
+                    "placeholder" => "Tapez une URL d'image !"
+                ]
+            ])
             ->add("category", EntityType::class, [
                 "label" => "Catégorie",
                 "placeholder" => "--Choisir une catégorie--",
@@ -85,6 +100,24 @@ class ProductController extends AbstractController
             ]); 
 
         $form = $builder->getForm();
+
+        $form->handleRequest($request);
+
+      if ($form->isSubmitted()) {
+        $product = $form->getData();
+        $product->setSlug(strtolower($slugger->slug($product->getName())));
+
+        // $product = new Product;
+        // $product->setName($data["name"])
+        //     ->setShortDescription($data["shortDescription"])
+        //     ->setPrice($data["price"])
+        //     ->setCategory($data["category"]);
+        $em->persist($product);
+        $em->flush();
+    
+      }
+
+       
 
         $formView = $form->createView();
 
