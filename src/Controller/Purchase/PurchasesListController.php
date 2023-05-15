9542e9de93f3cf13790eadaa71f622819fcf05c4
cedric-cochard <c.jc@live.fr>
2023-05-15 02:13:53 +0200

    refactoring class purchases list

--- src/Controller/Purchase/PurchasesListController.php
+++ src/Controller/Purchase/PurchasesListController.php
@@ -5,44 +5,31 @@ namespace App\Controller\Purchase;
 use App\Entity\User;
 use Twig\Environment;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class PurchasesListController extends AbstractController {
 
-  protected $security;
-  protected $router;
-  protected $twig;
-
-  public function __construct(Security $security, RouterInterface $router, Environment $twig) {
-    $this->security = $security;
-    $this->router = $router;
-    $this->twig = $twig;
-  }
-
   /**
    * Undocumented function
    *@Route("/purchases"), name="purchase_index"
+   *@IsGranted("ROLE_USER", message="Vous devez être connecté pour accèder à vos commandes")
    */
   public function index() {
     /**
      * @var User
      */
-    $user = $this->security->getUser();
+    $user = $this->getUser();
 
-    if (!$user) {
-     throw new AccessDeniedException("Vous devez être connecté pour accèder à vos commandes");
-    }
-
-    $html = $this->twig->render("purchase/index.html.twig", [
+   
+    return $this->render("purchase/index.html.twig", [
       "purchases" => $user->getPurchases()
     ]);
-
-    return new Response($html);
   }
 
 }
